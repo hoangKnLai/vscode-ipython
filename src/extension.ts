2fd79358ec95c16f1542a5e8691e793b2689cc8b
shaperilio <1@emil.io>
2022-05-27 18:50:46 -0700

    BF: pickup value from config

--- src/extension.ts
+++ src/extension.ts
@@ -58,6 +58,7 @@ export function activate(context: vscode.ExtensionContext) {
 		let cellFlag = config.get('cellTag') as string;
 		cellPattern = new RegExp(`^(?:${cellFlag.replace(' ', '\\s*')})`);
 		execLagMilliSec = config.get('execLagMilliSec') as number;
+		ipythonLagMillisec = config.get('ipythonLagMillisec') as number;
 		console.log('Cell Flag: ' + cellFlag);
 		launchArgs = config.get('launchArgs') as string;
 		startupCmds = config.get('startupCommands') as string[];
