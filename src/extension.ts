08dbcd89cf13e74168441874fb850a61d7664ed7
hoangKnLai <hoang.kn.lai@gmail.com>
2022-09-03 18:42:02 -0400

    WIP: improve cell handling

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { privateEncrypt } from 'crypto';
 import * as vscode from 'vscode';
 
 // === CONSTANTS ===
@@ -34,7 +35,7 @@ function getConfig(name:string){
     return config.get(name);
 }
 
-
+// NOTE: don't need when not using `%load`
 // function checkEncodingTag(document: vscode.TextDocument){
 // 	// REF: https://docs.python.org/3/reference/lexical_analysis.html#encoding-declarations
 //  let encodePattern = new RegExp('coding[=:]\\s*([-\\w.]+)');
@@ -50,11 +51,18 @@ function getConfig(name:string){
 // }
 
 
-function moveAndRevealCursor(line: number, editor: vscode.TextEditor){
-    let position = editor.selection.start.with(line, 0);
-    let cursor = new vscode.Selection(position, position);
-    editor.selection = cursor;
-    editor.revealRange(cursor.with(), vscode.TextEditorRevealType.InCenterIfOutsideViewport);
+function moveAndRevealCursor(
+    editor: vscode.TextEditor,
+    line: number,
+    char=0,
+    ){
+
+    let position = editor.selection.start.with(line, char);
+    let selection = new vscode.Selection(position, position);
+    editor.selection = selection;
+    editor.revealRange(
+        selection.with(),
+        vscode.TextEditorRevealType.InCenterIfOutsideViewport);
 }
 
 
@@ -89,6 +97,103 @@ function leftAdjustTrim(lines:string[]){
     return trimLines;
 }
 
+
+function getCellPattern(){
+    // RegExp to find and extract spaces in front of cell flag
+    let cellFlag = getConfig('CellTag') as string;
+    return new RegExp(`^([\\t ]*)(?:${cellFlag})`);
+}
+
+
+function getPythonEditor(){
+    let editor = vscode.window.activeTextEditor;
+    if (editor === undefined){
+        console.error('Unable to access Active Text Editor');
+        return;
+    }
+    if (editor.document.languageId !== 'python'){
+        console.error('Command only support "python" .py file');
+        return;
+    }
+    return editor;
+}
+
+
+/**
+ * Find cell tag line and level starting at cursor line and go toward beginning
+ * of file.
+ *
+ * @param aLevel - desired equal or higher level of the cell to find
+ * @returns [line, level] the line the cell is found and its level
+ */
+function findCellAboveCursor(
+    aLevel:number | undefined,
+){
+
+    let cellPattern = getCellPattern();
+    let editor = getPythonEditor();
+    if (editor === undefined){
+        return;
+    }
+    let startLine = editor.selection.start.line;
+    // let lineCount = editor.document.lineCount;
+
+    let cellLine = 0;  // beginning of file or first cell
+    let cellLevel = 0;  // no char before cell block
+    for (let iLine = startLine; iLine > 0; iLine--){
+        let line = editor.document.lineAt(iLine).text;
+        let found =  line.match(cellPattern);
+        if (found){
+            let level = found[1].replace('\t', '....').length;
+            if (aLevel !== undefined && level > aLevel){
+                continue;
+            }
+            cellLine = iLine;
+            cellLevel = level;
+            break;
+        }
+    }
+    return [cellLine, cellLevel] as const;
+
+}
+
+/**
+ * Find cell tag line and level starting at cursor line + 1 toward end of file.
+ *
+ * @param aLevel - desired equal or higher level of the cell to find
+ * @returns [line, level] the line the cell is found and its level
+ */
+function findCellBelowCursor(
+    aLevel:number | undefined,
+){
+
+    let cellPattern = getCellPattern();
+    let editor = getPythonEditor();
+    if (editor === undefined){
+        return;
+    }
+    let startLine = editor.selection.start.line;
+    let lineCount = editor.document.lineCount;
+
+    let cellLine = lineCount - 1;  // end of file or last cell
+    let cellLevel = 0;  // no char before cell block
+    for (let iLine = startLine + 1; iLine < lineCount; iLine++){
+        let line = editor.document.lineAt(iLine).text;
+        let found =  line.match(cellPattern);
+        if (found){
+            let level = found[1].replace('\t', '....').length;
+            if (aLevel !== undefined && level > aLevel){
+                continue;
+            }
+            cellLine = iLine;
+            cellLevel = level;
+            break;
+        }
+    }
+    return [cellLine, cellLevel] as const;
+}
+
+
 // === MAIN ===
 export function activate(context: vscode.ExtensionContext) {
     // Activation of this extension
@@ -103,64 +208,54 @@ export function activate(context: vscode.ExtensionContext) {
     // === LOCAL HELPERS ===
     function getIpyCommand(
         document: vscode.TextDocument,
-        selection: vscode.Selection | undefined){
+        selection: vscode.Selection){
 
         let nExec = 0;  // Number of execution needed on IPython console
         let cmd = '';
         document.save();  // force saving to properly use IPython %load
-        if (selection === undefined){
-            // -- Whole File
-            // REF: https://ipython.readthedocs.io/en/stable/interactive/magics.html#magic-run
-            cmd = `%run "${document.fileName}"`;
+        // -- Single line
+        if (selection.isSingleLine){
+            let text:string = '';
+            if (selection.isEmpty){  // support: run line at cursor when empty selection
+                text = document.lineAt(selection.start.line).text;
+            } else {
+                text = document.getText(selection.with());
+            }
+            cmd = `${text.trim()}`;
             nExec = 1;
             return {cmd, nExec};
-        }else{
-            // -- Single line
-            if (selection.isSingleLine){
-                let text:string = '';
-                if (selection.isEmpty){  // support: run line at cursor when empty selection
-                    text = document.lineAt(selection.start.line).text;
-                } else {
-                    text = document.getText(selection.with());
-                }
-                cmd = `${text.trim()}`;
-                nExec = 1;
-                return {cmd, nExec};
+        }
+
+        // -- Stack multiple consecutive lines
+        let textLines = document.getText(selection.with()).split(newLine);
+        const isNotEmpty = (item:string) => item.trim().length > 0;
+        let startLine = selection.start.line;
+        let startIndex = textLines.findIndex(isNotEmpty);
+        if (startIndex !== -1){
+            startLine += startIndex;
+        } else {  // all lines and partial lines are whitespaces
+            cmd = '';
+            nExec = 0;
+            return {cmd, nExec};
+        }
+        // NOTE: will use first non-empty line and include the whole line
+        // even if it is partially selected
+        let start = selection.start.with(startLine, 0);
+        let range = selection.with(start);
+
+        textLines = document.getText(range).split(newLine);
+
+        textLines = leftAdjustTrim(textLines);
+        if (textLines.length > 0){
+            cmd = textLines.join(newLine);
+            nExec = 1;  // extra exec to handle hanging block like `else:`
+
+            let lastIndex = textLines.length - 1;
+            let firstChar = textLines[lastIndex].search(/\S|$/);
+            if (firstChar > 0){  // last line is part of a block
+                nExec = 2;
             }
-
-            // -- Stack multiple consecutive lines
-            let textLines = document.getText(selection.with()).split(newLine);
-            const isNotEmpty = (item:string) => item.trim().length > 0;
-            let startLine = selection.start.line;
-            let startIndex = textLines.findIndex(isNotEmpty);
-            if (startIndex !== -1){
-                startLine += startIndex;
-            } else {  // all lines and partial lines are whitespaces
-                cmd = '';
-                nExec = 0;
-                return {cmd, nExec};
-            }
-            // NOTE: will use first non-empty line and include the whole line
-            // even if it is partially selected
-            let start = selection.start.with(startLine, 0);
-            let range = selection.with(start);
-
-            textLines = document.getText(range).split(newLine);
-
-            // textLines = textLines.filter(isNotEmpty);
-            textLines = leftAdjustTrim(textLines);
-            if (textLines.length > 0){
-                cmd = textLines.join(newLine);
-                nExec = 1;  // extra exec to handle hanging block like `else:`
-
-                let lastIndex = textLines.length - 1;
-                let firstChar = textLines[lastIndex].search(/\S|$/);
-                if (firstChar > 0){  // last line is part of a block
-                    nExec = 2;
-                }
-
-                return {cmd, nExec};
-            }
+            return {cmd, nExec};
         }
         return {cmd, nExec};
     }
@@ -174,8 +269,8 @@ export function activate(context: vscode.ExtensionContext) {
         if (cmd.length > 0){
             terminal.show(true);  // preserve focus
 
-            // No newLine to execute since IPython is trippy with when/how to
-            // execute a code line, block, multi-lines/blocks, etc.
+            // No newLine in sendText to execute since IPython is trippy with
+            // when/how to execute a code line, block, multi-lines/blocks, etc.
             terminal.sendText(cmd, false);  // false: no append `newline`
             console.log(`Command sent to terminal`);
 
@@ -200,7 +295,13 @@ export function activate(context: vscode.ExtensionContext) {
 
         // -- Create and Tag IPython Terminal
         await vscode.commands.executeCommand('python.createTerminal');
-        await vscode.commands.executeCommand('workbench.action.terminal.renameWithArg', {name : terminalName});
+        wait(200);  // msec, to help with a race condition not naming terminal
+        await vscode.commands.executeCommand(
+            'workbench.action.terminal.renameWithArg',
+            {name : terminalName}
+        );
+        wait(500);  // msec, to help with a race condition not naming terminal
+
         let terminal = vscode.window.activeTerminal as vscode.Terminal;
 
         // Launch options
@@ -223,8 +324,10 @@ export function activate(context: vscode.ExtensionContext) {
         }
         console.log('Startup Command: ', startupCmd);
         await execute(terminal, cmd);
-        await wait(1000);  // IPython takes awhile to load
-        await vscode.commands.executeCommand('workbench.action.terminal.scrollToBottom');
+        await wait(1000);  // IPython may take awhile to load
+        await vscode.commands.executeCommand(
+            'workbench.action.terminal.scrollToBottom'
+        );
         return terminal;
     }
 
@@ -297,7 +400,6 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
 
-    // -- Run a Selected Group of Text or Lines
     async function runSelections(){
         console.log('IPython run selection...');
         let editor = vscode.window.activeTextEditor;
@@ -334,10 +436,8 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
 
-    //-- Run a Cell
     async function runCell(isNext: boolean){
-        let cellFlag = getConfig('CellTag') as string;
-        let cellPattern = new RegExp(`^(?:${cellFlag})`);
+        // let cellPattern = getCellPattern();
 
         console.log('IPython run cell...');
         let editor = vscode.window.activeTextEditor;
@@ -350,25 +450,20 @@ export function activate(context: vscode.ExtensionContext) {
             return;
         }
 
-        let startLine = editor.selection.start.line;
-        let cellStart = 0;  // beginning of file
-        for (let iLine = startLine; iLine > 0; iLine--){
-            let line = editor.document.lineAt(iLine).text;
-            if (line.trim().match(cellPattern)){
-                cellStart = iLine;
-                break;
-            }
+        const cellAbove = findCellAboveCursor(undefined);
+        if (cellAbove === undefined){
+            console.error('Failed to find Cell');
+            return;
         }
+        let cellStart = cellAbove[0];
+        let cellLevel = cellAbove[1];
 
-        let lineCount = editor.document.lineCount;
-        let cellStop = lineCount - 1;  // end of file
-        for (let iLine = startLine + 1; iLine < lineCount; iLine++){
-            let line = editor.document.lineAt(iLine).text;
-            if (line.trim().match(cellPattern)){
-                cellStop = iLine;
-                break;
-            }
+        const cellBelow = findCellBelowCursor(cellLevel);
+        if (cellBelow === undefined){
+            console.error('Failed to find Cell');
+            return;
         }
+        let cellStop = cellBelow[0];
 
         let start = new vscode.Position(cellStart, 0);
         let stop = new vscode.Position(cellStop, 0);
@@ -388,10 +483,13 @@ export function activate(context: vscode.ExtensionContext) {
             await vscode.commands.executeCommand('workbench.action.terminal.scrollToBottom');
         }
 
-        let isNotLast = (cellStop + 1) < lineCount;
-        if (isNext && isNotLast){
-            let nextCell = cellStop + 1;
-            moveAndRevealCursor(nextCell, editor);
+        if (isNext){
+            let lineCount = editor.document.lineCount;
+            let isNotLast = (cellStop + 1) < lineCount;
+            if (isNotLast){
+                let nextCell = cellStop + 1;
+                moveAndRevealCursor(editor, nextCell);
+            }
         }
     }
 
@@ -431,21 +529,68 @@ export function activate(context: vscode.ExtensionContext) {
         }
     }
 
+
+    // TODO: add
+    //  - moveToPreviousCell(), moveToNextCell()
+
+
     // -- Register Command to Extension
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.createTerminal', createTerminal));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runFile', runFile));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runFileWithArgs', ()=>{runFile(false, true, false);}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runFileWithCli', ()=>{runFile(false, false, true);}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runFileWithArgsCli', ()=>{runFile(false, true, true);}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.resetAndRunFile', ()=>{runFile(true, false, false);}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.resetAndRunFileWithArgs', ()=>{runFile(true, true, false);}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.resetAndRunFileWithCli', ()=>{runFile(true, false, true);}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.resetAndRunFileWithArgsCli', ()=>{runFile(true, true, true);}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runSelections', runSelections));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runCell', runCell));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runCellAndMoveToNext', ()=>{runCell(true);}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runToLine', ()=>{runCursor('top');}));
-    context.subscriptions.push(vscode.commands.registerCommand('ipython.runFromLine', ()=>{runCursor('bottom');}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.createTerminal',
+            createTerminal));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runFile',
+            runFile));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runFileWithArgs',
+            ()=>{runFile(false, true, false);}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runFileWithCli',
+            ()=>{runFile(false, false, true);}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runFileWithArgsCli',
+            ()=>{runFile(false, true, true);}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.resetAndRunFile',
+            ()=>{runFile(true, false, false);}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.resetAndRunFileWithArgs',
+            ()=>{runFile(true, true, false);}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.resetAndRunFileWithCli',
+            ()=>{runFile(true, false, true);}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.resetAndRunFileWithArgsCli',
+            ()=>{runFile(true, true, true);}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runSelections',
+            runSelections));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runCell',
+            runCell));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runCellAndMoveToNext',
+            ()=>{runCell(true);}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runToLine',
+            ()=>{runCursor('top');}));
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'ipython.runFromLine',
+            ()=>{runCursor('bottom');}));
 
     // -- FIXME: Keybinding `when clause`
     vscode.commands.executeCommand('setContext', 'ipython.isUse', true);
